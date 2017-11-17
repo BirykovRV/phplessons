1ca2e16f258a02c4d9b490ae59d6261c63847892
Roman Birukov <r.v.birukov@yandex.ru>
2017-11-17 09:26:41 +0300

    ini

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <a href="reg_view.php">Регистрация</a><br>
     <a href="auth_view.php">Авторизация</a><br>
 		<?php else: ?>
-		<p><?php echo "Добро пожаловать, " . $_SESSION['username']; ?></p>
+		<p><?php echo "Добро пожаловать, " . $_SESSION['username'] . "."; ?></p>
 		<a href="logout.php" >Выход</a><br>
 		<?php endif ?>
 		<a href="callback_view.php">Форма обратной связи</a>
