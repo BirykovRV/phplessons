515d4922c3e2c3e68c7bfef2e9588188369bc7d0
Roman Birukov <r.v.birukov@yandex.ru>
2017-11-18 16:03:43 +0300

    Add article script

--- articles.php
+++ articles.php
@@ -3,6 +3,6 @@ require_once 'dbconnection.php';
 $db->Connect();
 $data = $_POST;
 if (isset($data['create_article'])) {
-  header('Refresh: 3; url="http://phplessons:8080/add_article.php"');
+  header('Refresh: 2; url="http://phplessons:8080/add_article.php"');
   echo "Статья добавлена!";
 }
