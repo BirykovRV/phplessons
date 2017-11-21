6151849a7788ecc28826fadce0541957929f4ea8
Roman Birukov <r.v.birukov@yandex.ru>
2017-11-21 11:34:04 +0300

    css

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ arsort($articles);
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/master.css">
     <title>Главное меню</title>
   </head>
@@ -22,7 +24,8 @@ arsort($articles);
     <a href="add_article.php">Добавить статью</a><br>
 		<a href="logout.php" >Выход</a><br>
 		<?php endif ?>
-		<a href="callback_view.php">Форма обратной связи</a>
+		<a href="callback_view.php">Форма обратной связи</a><br>
+    <a href="Ajax.html">Тесты Ajax</a>
 
 
       <!-- проходим по массиву и выводим статьи -->
@@ -31,7 +34,7 @@ arsort($articles);
         <?php $user = $db->GetUserById(array($value['userid'])); ?>
         <div class="articles">
           <h1><?php echo $value['title']; ?></h1>
-          <p style="width: 700px;"><?php echo $value['article']; ?></p>
+          <p><?php echo $value['article']; ?></p>
           <div class="">
             <h5>Создан: <?php echo $value['created'] . " / Автор: " . $user['login']; ?></h5>
           </div>
