647cf5166caac1af5e56ee700f240f74e2c04aef
Roman Birukov <r.v.birukov@yandex.ru>
2017-11-21 08:53:34 +0300

    стили

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ arsort($articles);
 <html>
   <head>
     <meta charset="utf-8">
-
+    <link rel="stylesheet" href="/css/master.css">
     <title>Главное меню</title>
   </head>
   <body>
@@ -24,17 +24,21 @@ arsort($articles);
 		<?php endif ?>
 		<a href="callback_view.php">Форма обратной связи</a>
 
-    <!-- проходим по массиву и выводим статьи -->
-    <?php foreach ($articles as $key => $value): ?>
-      <!-- находим пользователя по id -->
-      <?php $user = $db->GetUserById(array($value['userid'])); ?>
 
-        <h1><?php echo $value['title']; ?></h1>
-        <p style="width: 700px;"><?php echo $value['article']; ?></p>
-        <div class="">
-          <h5>Создан: <?php echo $value['created'] . " / Автор: " . $user['login']; ?></h5>
+      <!-- проходим по массиву и выводим статьи -->
+      <?php foreach ($articles as $key => $value): ?>
+        <!-- находим пользователя по id -->
+        <?php $user = $db->GetUserById(array($value['userid'])); ?>
+        <div class="articles">
+          <h1><?php echo $value['title']; ?></h1>
+          <p style="width: 700px;"><?php echo $value['article']; ?></p>
+          <div class="">
+            <h5>Создан: <?php echo $value['created'] . " / Автор: " . $user['login']; ?></h5>
+          </div>
         </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
+
+
 
     <script src="http://localhost:35729/livereload.js" charset="utf-8"></script>
   </body>
